d32c6d889fbb0fd4b17bb953f1ed16bdc883bb8e
Ali Almahdi <ali@almahdi.cc>
2022-06-18 15:22:31 +0000

    testing formly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { FormlyModule } from '@ngx-formly/core'; 
+import { FormlyMaterialModule } from '@ngx-formly/material';
 import { MainPageComponent } from './pages/main/main.component';
 
 @NgModule({
@@ -14,6 +17,7 @@ import { MainPageComponent } from './pages/main/main.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule, FormlyModule.forRoot(), FormlyMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,8 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormlyFieldConfig, FormlyModule } from "@ngx-formly/core";
+import { FormlyMaterialModule } from "@ngx-formly/material";
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './main.component.html',
     standalone: true,
+    imports: [
+        ReactiveFormsModule, FormlyModule, FormlyMaterialModule,MatFormFieldModule,
+    ]
 })
-export class MainPageComponent {};
+export class MainPageComponent implements OnInit {
+    form = new FormGroup({});
+    model = { email: 'email@email.com' };
+    fields: FormlyFieldConfig[] = [
+        {
+            key: 'email',
+            type: 'input',
+        }
+    ]
+
+    constructor() {}
+    ngOnInit(): void {
+        
+    }
+    onSubmit(model: any) {
+        console.log({model});
+    }
+
+};
