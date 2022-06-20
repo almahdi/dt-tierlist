5ed2e57d05543a0848aff933d1c87736cd682130
Ali Almahdi <ali.almahdi@gmail.com>
2022-06-20 10:14:30 +0300

    flake lock

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
         buildInputs = with pkgs; [
           nodejs
           jq
-          nodePackages.prisma
           nodePackages.yarn
           nodePackages.prettier
         ];
@@ -25,11 +24,6 @@
           export PATH="$PWD/node_modules/.bin/:$PATH"
           alias scripts='jq ".scripts" package.json'
           export PS1="(nodejs) \[\033[1;32m\][\u@\h:\w]\$\[\033[0m\]"
-          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
-          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
           npm set prefix ~/.npm-global
           export PATH=$PATH:~/.npm-global/bin
         '';
